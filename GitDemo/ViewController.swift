f52a6b7eda5d8a96e25e81d685cb342b37c677d2
ivanmas97 <ivanmas97@yandex.ru>
2023-05-03 17:13:55 +0300

    Initial commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Ivan Maslov on 03.05.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
